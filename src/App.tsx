b8dfccb12bde555ffa0f5716604bcbd62f9b7340
FHNHajid <farhan.hajid101@gmail.com>
2025-03-18 22:12:54 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './component/navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <h1>Trashure</h1>
     </div>
   )
