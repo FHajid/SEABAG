ba81b27926f806779e11293a6d3d89c7f6f38e77
FHNHajid <farhan.hajid101@gmail.com>
2025-03-25 22:05:57 +0700

    updated

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer () {
     return (
         <footer className="bg-gray-100 p-4">
-            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <div className="container mx-auto flex flex-col md:flex-row justify-center items-center">
                 <div className="text-gray-700 text-center md:text-left mb-2 md:mb-0">
                     Your Text Here
                 </div>
