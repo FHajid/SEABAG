c13721a22a213e83268d4c4375f3cb351f7692b5
FHNHajid <farhan.hajid101@gmail.com>
2025-05-19 22:14:05 +0700

    upda'6re

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,8 +1,20 @@
 
 export default function news() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="mb-4">
+        <label typeof="email" className="block text-sm font-medium text-gray-700 mb-2">Email Address</label>
+        <input type="email" id="email" placeholder="Enter your email..." className="mt-2 block w-full rounded-md shadow-sm border-gray-100 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"></input>
+      </div>
+
+      <button type="submit" className="block bg-indigo-600 text-white px-8 py-3 rounded-md text-lg font-medium hover:bg-indigo-700 transition-colors duration-300">
+        Send Message
+      </button>
+      <div>
+        <p className="text-gray-600 mt-4 py-2">
+          This is a sample contact page. Feel free to reach out to me at [email].
+        </p>
+      </div>
+    </>
   )
 }
